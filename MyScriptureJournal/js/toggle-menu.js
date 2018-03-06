1992fdee1e386161155db5da36009797fc418eb3
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-03-06 13:04:30 -0700

    Finalized Commit

---
+++ MyScriptureJournal/js/toggle-menu.js
@@ -0,0 +1,11 @@
+﻿// Toggles the menu - reference from Sliding Menu assignment in WDD331 Advanced CSS
+function toggleNav() {
+    var slideNav = document.getElementById("menu-list");
+    // toggle adds a class if it's not there or removes it if it is.
+    slideNav.classList.toggle("open");
+}
+
+// Toggles the classes of the menu icon, which will change between the hamburger and X icons
+function hamSwitch(x) {
+    x.classList.toggle("ham-switch");
+}
